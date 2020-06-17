aa50712a5f9f2ff58fd4418d3d4c7d9ddf112554
ge34diy <ge34diy@mytum.de>
2020-06-17 23:45:33 +0200

    coma dataset

--- datasets/coma_dataset.py
+++ datasets/coma_dataset.py
@@ -11,120 +11,112 @@ from utils.coma.utils import get_vert_connectivity
 from utils.coma.transform_clsf import Normalize
 
 class ComaDataset(InMemoryDataset):
-    def __init__(self, data_path, dtype='train', split='clsf', split_term='clsf', nVal = 0, transform=None, pre_transform=None):
+    def __init__(self, data_path, dtype='train', split='clsf', transform=Normalize(), pre_transform=None):
         self.data_path = data_path
         self.dtype = dtype
         self.split = split
-        self.split_term = split_term
-        self.nVal = nVal
         self.transform = transform
         self.pre_tranform = pre_transform
+        self.processed_dir = './processed/coma/'
         # Downloaded data is present in following format root_dir/*/*/*.py
-        self.data_file = self.gather_paths(self.split_term)
+        self.filepaths, self.categories = self.gather_paths()
         super(ComaDataset, self).__init__(data_path, transform, pre_transform)
-        
+
         if dtype == 'train':
             data_path = self.processed_paths[0]
         elif dtype == 'test':
             data_path = self.processed_paths[1]
         else:
-            raise Exception("train, val and test are supported data types")
+            raise Exception("train and test are supported data types")
 
         norm_path = self.processed_paths[2]
         norm_dict = torch.load(norm_path)
         self.mean, self.std = norm_dict['mean'], norm_dict['std']
         self.data, self.slices = torch.load(data_path)
-        if self.transform:
+
+        if self.transform is not None:
+            if hasattr(self.transform, 'mean') and hasattr(self.transform, 'std'):
+                if self.tranform.mean is None:
+                    self.tranform.mean = self.mean
+                if self.transform.std is None:
+                    self.tranform.std = self.std
             self.data = [self.transform(td) for td in self.data]
 
     @property
     def raw_file_names(self):
-        all_data_files = []
-        for key in self.data_file.keys() :
-            all_data_files += self.data_file[key]
-        return all_data_files
+        all_fps = []
+        for key in self.filepaths.keys() :
+            all_fps += self.filepaths[key]
+        return all_fps
 
     @property
     def processed_file_names(self):
         processed_files = ['training.pt', 'test.pt', 'norm.pt']
-        processed_files = [self.split_term+'_'+pf for pf in processed_files]
+        processed_files = [self.split+'_'+pf for pf in processed_files]
         return processed_files
 
-    def gather_paths(self, st):
-        datapaths = dict()
-        count = 0
-            datapaths['ad'] = dict()
-            datapaths['cn'] = dict()
-            datapaths['ad']['train'] = []
-            datapaths['ad']['test'] = []
-            datapaths['cn']['train'] = []
-            datapaths['cn']['test'] = []
+    def gather_paths(self):
+        filepaths = dict()
 
-            imgids = np.load('data/adni2_clsfb_dict.npy', allow_pickle=True)
-            imgids = imgids.item()
-            for dx in imgids.keys() :
-                for tt in imgids[dx].keys() :
-                    for i in imgids[dx][tt] :
-                        datapaths[dx][tt].append(self.root_dir+'/'+i+'-L_Hipp_first.obj')
-                        count += 1
-        print('total number of dataset: %d'%(count))
-        return datapaths
+        df = pd.read_csv(self.data_path)
+        fps = df['mesh_fsl']
+        dxs = df['dx']
+        categories = sorted(set(dxs))
+        for i in range(len(df)) :
+            if dxs[i] in datapaths.keys() :
+                filepaths[dxs[i]] = []
+                filepaths[dxs[i]].append(fps[i])
+            else :
+                filepaths[dxs[i]].append(fps[i])
+
+        print('total number of dataset: %d'%(len(df)))
+        for key in filepaths.keys() :
+            print('%s: %d.'%(key, len(filepaths[key])))
+        return filepaths, categories
 
     def process(self):
-        train_data, val_data, test_data = [], [], []
-        train_vertices = []
-        for dx in self.data_file :
-            for tt in self.data_file[dx] :
-                for f in tqdm(self.data_file[dx][tt]):
-                    mesh = Mesh(filename=f)
-                    mesh_verts = torch.Tensor(mesh.v)
-                    adjacency = get_vert_connectivity(mesh.v, mesh.f).tocoo()
-                    edge_index = torch.Tensor(np.vstack((adjacency.row, adjacency.col)))
-                    if dx == 'ad' :
-                        data = Data(x=mesh_verts, y=torch.Tensor([1,0]), edge_index=edge_index)
-                    elif dx == 'cn' :
-                        data = Data(x=mesh_verts, y=torch.Tensor([0,1]), edge_index=edge_index)
+        dataset = []
+        vertices = []
+        for dx in self.filepaths :
+            for fp in self.filepaths[dx] :
+                mesh = Mesh(filename=fp)
+                mesh_verts = torch.Tensor(mesh.v)
+                adjacency = get_vert_connectivity(mesh.v, mesh.f).tocoo()
+                edge_index = torch.Tensor(np.vstack((adjacency.row, adjacency.col)))
 
-                    if tt == 'test' :
-                        test_data.append(data)
-                    elif tt == 'train' :
-                        train_data.append(data)
-                        train_vertices.append(mesh.v)
+                i = self.categories.index(dx)
+                label = np.zeros(len(self.categories))
+                label[i] = 1
+                data = Data(x=mesh_verts, y=torch.Tensor(label), edge_index=edge_index)
 
-        mean_train = torch.Tensor(np.mean(train_vertices, axis=0))
-        std_train = torch.Tensor(np.std(train_vertices, axis=0))
-        norm_dict = {'mean': mean_train, 'std': std_train}
-        if self.pre_transform is not None:
-            if hasattr(self.pre_transform, 'mean') and hasattr(self.pre_transform, 'std'):
-                if self.pre_tranform.mean is None:
-                    self.pre_tranform.mean = mean_train
-                if self.pre_transform.std is None:
-                    self.pre_tranform.std = std_train
-            train_data = [self.pre_transform(td) for td in train_data]
-            val_data = [self.pre_transform(td) for td in val_data]
-            test_data = [self.pre_transform(td) for td in test_data]
+                dataset.append(data)
+                vertices.append(mesh.v)
 
-        torch.save(self.collate(train_data), self.processed_paths[0])
-        #torch.save(self.collate(val_data), self.processed_paths[1])
-        torch.save(self.collate(test_data), self.processed_paths[1])
-        torch.save(norm_dict, self.processed_paths[2])
+        if self.dtype == 'train' :
+            mean_train = torch.Tensor(np.mean(vertices, axis=0))
+            std_train = torch.Tensor(np.std(vertices, axis=0))
+            norm_dict = {'mean': mean_train, 'std': std_train}
+            torch.save(self.collate(dataset), self.processed_paths[0])
+            torch.save(norm_dict, self.processed_paths[2])
+        elif self.dtype == 'test' :
+            torch.save(self.collate(test_data), self.processed_paths[1])
 
-def prepare_clsf_dataset(path, st):
-    ComaDataset(path, split='clsf', split_term=st, pre_transform=Normalize())
+
+def prepare_clsf_dataset(path):
+    ComaDataset(path, split='clsf', pre_transform=None, transform=None)
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='ADNI2 Data preparation for Convolutional Mesh Autoencoders')
     parser.add_argument('-s', '--split', default='clsf', help='split can be clsf')
-    parser.add_argument('-st', '--split_term', default='clsf', help='name of dataset')
     parser.add_argument('-d', '--data_path', help='path where the downloaded data is stored, or csv file contains information of data files')
 
     args = parser.parse_args()
     split = args.split
     data_path = args.data_dir
     if split == 'clsf':
-        prepare_clsf_dataset(data_path, args.spilt_term)
+        prepare_clsf_dataset(data_path)
     else:
         raise Exception("Only clsf split are supported")
 
